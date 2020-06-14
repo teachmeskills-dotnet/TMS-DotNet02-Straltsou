80814fd32c4814fc59446d84e43779748d2e9446
Straltsou Maxim <green1971weekend1@gmail.com>
2020-06-14 17:37:48 +0300

    feat: models added, prepared startup

---
+++ src/LearnApp.WebAPI/Models/YandexModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnApp.WebAPI.Models
+{
+    /// <summary>
+    /// Yandex translate model.
+    /// </summary>
+    public class YandexModel
+    {
+        /// <summary>
+        /// Code of language.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Language.
+        /// </summary>
+        public string Lang { get; set; }
+
+        /// <summary>
+        /// Words array.
+        /// </summary>
+        public string[] Text { get; set; }
+    }
+}
---
+++ src/LearnApp.WebAPI/Startup.cs
@@ -0,0 +1,52 @@
+using LearnApp.WebAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+
+namespace LearnApp.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                        options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                        options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                    });
+            services.Configure<ApiConfig>(Configuration.GetSection("API"));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
